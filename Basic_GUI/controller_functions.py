141ecf068416b196e2a7ab8e44af09722f8d438b
jdeleon-py <53531827+jdeleon-py@users.noreply.github.com>
2020-05-19 16:18:38 -0500

    Update controller_functions.py

--- Basic_GUI/controller_functions.py
+++ Basic_GUI/controller_functions.py
@@ -6,13 +6,36 @@ class Controller:
 		self.port = py.Arduino.AUTODETECT
 		self.board = py.Arduino(self.port)
 
-		self.dig_out = self.board.get_pin('d:2:o')
+		self.digital_out = self.board.get_pin('d:2:o')
+		self.analog_out = self.board.get_pin('d:3:p')
 
 		self.HIGH = 1
 		self.LOW = 0
 
+
+class DigitalIO(Controller):
+
+	def __init__(self):
+		self.controller = Controller()
+
 	def output_high(self):
-		return self.dig_out.write(self.HIGH)
+		return self.controller.digital_out.write(self.controller.HIGH)
 
 	def output_low(self):
-		return self.dig_out.write(self.LOW)
+		return self.controller.digital_out.write(self.controller.LOW)
+
+
+class AnalogIO(Controller):
+
+	def __init__(self):
+		self.controller = Controller()
+		self.controller.board.samplingOn()
+
+	def output_data(self, data):
+		return self.controller.analog_out.write(data / 1000)
+
+# GUI is analog input (scrollbar will control the output of an LED)
+# Do I have to convert between ADC and a voltage?
+
+# Scrollbar input will have values from 0-1023.
+# output PWM operates from 0-255 (input / 4)
